4dea3d780403717462caa8d78f6b8cc81a395775
Sen Lin <slin@jhu.edu>
2017-04-24 22:37:16 -0700

    Added logger and enabled cross origion resource sharing

--- spring-boot-backend/src/main/java/com/slin/todo/controller/ToDoController.java
+++ spring-boot-backend/src/main/java/com/slin/todo/controller/ToDoController.java
@@ -4,18 +4,23 @@ import com.slin.todo.domain.ToDo;
 import com.slin.todo.repository.ToDoRepository;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Log4j
 public class ToDoController {
   private final ToDoRepository toDoRepository;
 
+  @CrossOrigin
   @GetMapping("/todo")
   public Collection<ToDo> index() {
+    log.info("Received request for all todo");
     return toDoRepository.findAll();
   }
 
