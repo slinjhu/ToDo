ba4599b032a131d672f05e950374c3a33759345e
Sen Lin <slinjhu@gmail.com>
2017-04-25 15:55:29 -0700

    minor revisions.

--- frontend-backbone/js/todoApp.js
+++ frontend-backbone/js/todoApp.js
@@ -1,10 +1,9 @@
 var app = app || {};
 
-app.allToDos.fetch();
-
 var tasksView = new app.viewAllToDos({
 	collection: app.allToDos
 });
 
 $("#allToDos").html(tasksView.render().el);
 
+app.allToDos.fetch();
